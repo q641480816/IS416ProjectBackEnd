8fbfeb407c9cb0ffc22609f7f24bff78489b719f
Liu jinlong <q641480816@gmail.com>
2018-03-28 23:42:52 +0800

    s

--- src/websocket/WebSocketServer.java
+++ src/websocket/WebSocketServer.java
@@ -5,9 +5,9 @@
  */
 package websocket;
 
-import java.io.StringReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -15,55 +15,80 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 /**
  *
  * @author Jeffrey Pan
  */
 @ApplicationScoped
-@ServerEndpoint("/actions")
+@ServerEndpoint("/eventSocket/{acId}")
 public class WebSocketServer {
-    
-    @Inject
-    private SessionHandler sessionHandler;
-    
+
+    private static int onlineCount = 0;
+    private static Map<Long, WebSocketServer> clients = new ConcurrentHashMap<Long, WebSocketServer>();
+    private Session session;
+    private long id;
+
     @OnOpen
-    public void open(Session session) {
-        sessionHandler.addSession(session);
+    public void onOpen(@PathParam("acId") long id, Session session) throws IOException {
+
+        this.id = id;
+        this.session = session;
+
+        addOnlineCount();
+        clients.put(id, this);
+        System.out.println("Connected");
     }
 
     @OnClose
-    public void close(Session session) {
-        sessionHandler.removeSession(session);
-    }
-
-    @OnError
-    public void onError(Throwable error) {
-        Logger.getLogger(WebSocketServer.class.getName()).log(Level.SEVERE, null, error);
+    public void onClose() throws IOException {
+        clients.remove(id);
+        subOnlineCount();
     }
 
     @OnMessage
-        public void handleMessage(String message, Session session) {
-            try (JsonReader reader = Json.createReader(new StringReader(message))) {
-            JsonObject jsonMessage = reader.readObject();
+    public void onMessage(String message) throws IOException {
+        sendMessageTo(message, id);
+    }
 
-            if ("add".equals(jsonMessage.getString("action"))) {
-                //add new user to the server session
-                //sessionHandler.addParticipants(user);
-            }
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
 
-            if ("remove".equals(jsonMessage.getString("action"))) {
-                //remove user from the server session
-                //long id = (long) jsonMessage.getInt("account_id");
-                //User user = sessionHandler.getParticipantById(id);
-                //sessionHandler.removeParticipant(user);
-            }
+    public void sendMessageTo(String message, long To) throws IOException {
+        // session.getBasicRemote().sendText(message);
+        //session.getAsyncRemote().sendText(message);
+        for (WebSocketServer item : clients.values()) {
+            if (item.id  == To )
+                item.session.getAsyncRemote().sendText(message);
         }
+    }
+
+    public void sendMessageAll(String message) throws IOException {
+        for (WebSocketServer item : clients.values()) {
+            item.session.getAsyncRemote().sendText(message);
+        }
+    }
+
+
+
+    public static synchronized int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public static synchronized void addOnlineCount() {
+        WebSocketServer.onlineCount++;
+    }
+
+    public static synchronized void subOnlineCount() {
+        WebSocketServer.onlineCount--;
+    }
+
+    public static synchronized Map<Long, WebSocketServer> getClients() {
+        return clients;
     }
 }
